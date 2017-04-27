43b1d99018db10a6b41a51b6919002948a16822a
Archetype80 <archetype80@hotmail.com>
2017-04-28 04:10:21 +1000

    Added several prefabs and scripts

---
+++ projectRamRom/Assets/Scripts/Controller Components/BulletCollision.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletCollision : MonoBehaviour {
+	
+	void OnCollisionEnter(Collision collision)
+	{
+		var hit = collision.gameObject;
+		var health = hit.GetComponent<Health>();
+		if (health  != null)
+		{
+			health.TakeDamage(10);
+			transform.localScale = new Vector3(0.1F, 0.1F, 0.1F);
+		}
+	}
+}
---
+++ projectRamRom/Assets/Scripts/Controller Components/Health.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.Networking;
+
+public class Health : NetworkBehaviour {
+
+	public bool isAlive = true;
+
+	public const float maxHealth = 1000;
+	[SyncVar(hook = "OnChangeHealth")]
+	public float currentHealth = maxHealth;
+	public RectTransform healthBar;
+
+	public void TakeDamage(int amount)
+	{
+		if (!isServer)
+			return;
+
+		currentHealth -= amount;
+		if (currentHealth <= 0)
+		{
+			currentHealth = 0;
+			isAlive = false;
+			Debug.Log("Health on an object has been depleted");
+		}
+	}
+
+	void OnChangeHealth (float health)
+	{
+		healthBar.sizeDelta = new Vector2(GetHealthPercentage(), healthBar.sizeDelta.y);
+	}
+
+	public float GetHealthPercentage()
+	{
+		return (currentHealth / maxHealth) * 100;
+	}
+}
